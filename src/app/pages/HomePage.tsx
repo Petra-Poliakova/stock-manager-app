4ca1187ffa1ed4a9f2d5facb2b208eb119023441
Petra poliakova <petra.poliakova@dxc.com>
2024-07-15 15:59:59 +0200

    fix collapsible sidebar

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import LoadingSpinner from '../../components/LoadingSpinner'
 import { Header } from '../../components/Header'
-
 
 import './../../styles/globalStyle.scss'
 
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -6,36 +6,44 @@ import { IMAGES } from '../constans/constans';
 
 import "../styles/layouts/RootLayout.scss"
 
+
 interface RootLayoutProps {}
 
 const RootLayout: React.FC<RootLayoutProps> = () => {
   const { isFullSizeMenu } = useMenu();
 
+  const menuClasses = `sidebar ${isFullSizeMenu ? '' : 'collapsed'}`;
+  const menuStyle = { width: isFullSizeMenu ? '207px' : '75px' };
+  const mainStyle = { width: isFullSizeMenu ? 'calc(100% - 207px)' : 'calc(100% - 75px)' };
+
   return (
-    <div style={{ display:'flex', flexDirection:'row'}}>
-    <div className='sidebar' style={{width: isFullSizeMenu ? '207px' : '75px' }}>
-      <div className='logo'>
-        <IMAGES.Logo style={{width: isFullSizeMenu ? '75px' : '50px'}}/>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className={menuClasses} style={menuStyle}>
+        <div>
+          <IMAGES.Logo className={`logo ${isFullSizeMenu ? '' : 'collapsed'}`} />
+        </div>
+        <nav className="menu">
+          <NavLink to="/" className="navItem">
+            <IMAGES.DashboardIcon width="24px" height="24px" className="icon" />
+            {isFullSizeMenu && <span className="text">Dashboard</span>}
+          </NavLink>
+          <NavLink to="/products" className="navItem">
+            <IMAGES.ProductIcon width="24px" height="24px" className="icon" />
+            {isFullSizeMenu && <span className="text">Products</span>}
+          </NavLink>
+          <NavLink to="/support" className="navItem">
+            <IMAGES.SupportIcon width="24px" height="24px" className="icon" />
+            {isFullSizeMenu && <span className="text">Support</span>}
+          </NavLink>
+        </nav>
       </div>
-      {isFullSizeMenu ? 
-      <nav className='menu'>
-          <NavLink to="/" className='navItem'><IMAGES.DashboardIcon width='24px' height='24px' className='icon'/> Dashboard</NavLink>
-          <NavLink to="/products" className='navItem'><IMAGES.ProductIcon width='24px' height='24px' className='icon'/>Products</NavLink>
-          <NavLink to="/support" className='navItem'><IMAGES.SupportIcon width='24px' height='24px' className='icon'/> Support</NavLink>
-      </nav> : 
-      <nav className='menu'>
-        <NavLink to="/" className='navItem'><IMAGES.DashboardIcon width='24px' height='24px' className='icon'/></NavLink>
-        <NavLink to="/products" className='navItem'><IMAGES.ProductIcon width='24px' height='24px' className='icon'/></NavLink>
-        <NavLink to="/support" className='navItem'><IMAGES.SupportIcon width='24px' height='24px' className='icon'/></NavLink>
-      </nav>}
+
+      <main style={mainStyle}>
+        <Outlet />
+      </main>
     </div>
+  );
+};
 
-    <main style={{width:isFullSizeMenu ? 'calc(100% - 207px)' : 'calc(100% - 75px)'}}>
-      <Outlet />
-    </main>
-  </div>
-  )
-}
-
-export default RootLayout
+export default RootLayout;
 
