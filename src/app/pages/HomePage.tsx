bd4ce153398de47dc59d128d22524012c0558ac6
Petra poliakova <petra.poliakova@dxc.com>
2024-07-23 17:32:04 +0200

    added analytics data

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -6,14 +6,16 @@ import LoadingSpinner from 'components/LoadingSpinner'
 import { TData } from './products/Products'
 import { IMAGES } from 'constans/constans'
 
+import salesData from 'data/sales.json'
+
 import 'styles/globalStyle.scss'
 import './HomePage.scss'
 
 const HomePage = () => {
-  const {data, error, isLoading} = useFetch<TData[]>('https://dummyjson.com/products?limit=0',)
+  const {data, error, isLoading} = useFetch<TData>('https://dummyjson.com/products?limit=0',)
 
   console.log('Data HP', data)
-
+  
   if (isLoading) {
     return <LoadingSpinner />
   }
@@ -21,6 +23,17 @@ const HomePage = () => {
     return <div>Error: {error?.message}</div>
   }
 
+  const getFormatNumberWithDot = (number: number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+  }
+
+  const totalSalesQuantity = salesData.sales.map(item => item.quantitySold).reduce((a, b) => a + b, 0);
+  const totalSalesRevenue = salesData.sales.map(item => item.unitPrice * item.quantitySold).reduce((a, b) => a + b, 0);
+  const averageRevenuePerUnit = (totalSalesRevenue / totalSalesQuantity) * 100;
+
+  //const inStockProducts = data?.products.filter(product => product.availabilityStatus === 'In Stock');
+  const lowStockProducts = data?.products.filter(product => product.availabilityStatus === 'Low Stock');
+  //const outOfStockProducts = data?.products.filter(product => product.availabilityStatus === 'Out of Stock');
 
   return (
     <div className='page-container'>
@@ -29,10 +42,28 @@ const HomePage = () => {
         <div className='box'>
           <div className='box-text'>
             <span>New sales</span>
-            <span>1.355</span>
+            <span>{(getFormatNumberWithDot(Math.floor(totalSalesQuantity)))}</span>
           </div>
           <div className='box-icon'>
             <IMAGES.ChartBar/>
+          </div>
+        </div>
+        <div className='box'>
+          <div className='box-text'>
+            <span>New sales revenue</span>
+            <span>€{(getFormatNumberWithDot(Math.floor(totalSalesRevenue)))}</span>
+          </div>
+          <div className='box-icon'>
+            <IMAGES.Money/>
+          </div>
+        </div>
+        <div className='box'>
+          <div className='box-text'>
+            <span>Revenue per unit</span>
+            <span>{getFormatNumberWithDot(Math.floor(averageRevenuePerUnit))}%</span>
+          </div>
+          <div className='box-icon'>
+            <IMAGES.ChartLine/>
           </div>
         </div>
         <div className='box'>
@@ -44,27 +75,9 @@ const HomePage = () => {
             <IMAGES.UserPlus/>
           </div>
         </div>
-        <div className='box'>
-          <div className='box-text'>
-            <span>Income per leads</span>
-            <span>€1.910</span>
-          </div>
-          <div className='box-icon'>
-            <IMAGES.Money/>
-          </div>
-        </div>
-        <div className='box'>
-          <div className='box-text'>
-            <span>Conversion rate</span>
-            <span>3.51%</span>
-          </div>
-          <div className='box-icon'>
-            <IMAGES.ChartLine/>
-          </div>
-        </div>
+        
        
       </div>
-
     </div>
   )
 }
