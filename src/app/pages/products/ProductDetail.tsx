fa3223c022c871d9b0a5f7ac5818a850c3d56380
Petra Poliakova <petra.poliakova92@gmail.com>
2026-04-24 15:55:55 +0200

    Add delete product

--- src/app/pages/products/ProductDetail.tsx
+++ src/app/pages/products/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLoaderData, LoaderFunctionArgs } from "react-router";
+import { useLoaderData, LoaderFunctionArgs, useNavigate } from "react-router";
 import { Header } from "@/components/Header";
 
 import UniversalImg from '../../../assets/univesral-image.jpg';
@@ -65,7 +65,7 @@ export const ProductDetail = () => {
   const roundedAverageRating = Number(averageRating.toFixed(1));
   const roundedProductRating = Math.round(averageRating);
   const ratingDistribution = [5, 4, 3, 2, 1].map((star) => {
-    const count = reviews.filter((review) => Math.round(review.rating) === star).length;
+  const count = reviews.filter((review) => Math.round(review.rating) === star).length;
 
     return {
       star,
@@ -74,11 +74,36 @@ export const ProductDetail = () => {
     };
   });
 
+const navigate = useNavigate();
+
+
   const images = [
     productId.images[0] || UniversalImg,
     productId.images[1] || UniversalImg,
     productId.images[2] || UniversalImg,
   ]
+
+ const handleDeleteProduct = async () => {
+  try {
+    const response = await fetch(`https://dummyjson.com/products/${productId.id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete product");
+    }
+
+    const deletedProduct = await response.json();
+    console.log(deletedProduct);
+
+    alert(`Product ${productId.title}, id: ${productId.id} was deleted`);
+    navigate('/products');
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    alert("Deleting product failed.");
+  }
+};
+
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', gap: '25px', minHeight: '100vh', }} >
@@ -255,7 +280,7 @@ export const ProductDetail = () => {
               </div>
             </div>
           </div>
-          <div className="product-menu-footer">
+          <div className="product-menu-footer" onClick={handleDeleteProduct}>
             <div >
               <div><LuTrash2 size={26} color="#283455"/></div>
               <div>
